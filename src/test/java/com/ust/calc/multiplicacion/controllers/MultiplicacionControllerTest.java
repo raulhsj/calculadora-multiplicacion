e50d6a0e8f29709a49708b5d5fe79c76e62eeaea
Charly Chávez Ordóñez <nekperu15739@gmail.com>
2018-07-05 11:43:19 +0200

    Feature mutiplicacion pom.xml: added test dependency src/test/java/com/ust/calc/multiplicacion/controllers/MultiplicacionControllerTest.java: added test

--- src/test/java/com/ust/calc/multiplicacion/controllers/MultiplicacionControllerTest.java
+++ src/test/java/com/ust/calc/multiplicacion/controllers/MultiplicacionControllerTest.java
@@ -1,19 +1,30 @@
 package com.ust.calc.multiplicacion.controllers;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MultiplicacionControllerTest {
+
+    private Multiply multiplicacionController;
 
 	@Before
 	public void setUp() throws Exception {
+        multiplicacionController = new MultiplicacionController();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void happyPathTest() {
+        final Integer multi = multiplicacionController.doMulti(3, 2);
+        assertThat(multi).isEqualTo(6);
+    }
+
+    @Test
+    public void noHappyPathTest() {
+        final Integer multi = multiplicacionController.doMulti(3, 3);
+        assertThat(multi).isNotEqualTo(3);
+    }
+
 
 }
