d6b0579533ee3f4b078fe1a9186ac85f62177b4b
raulhsj <Raul.Hernandez@ust-global.com>
2018-07-05 10:57:26 +0200

    Commit para mi compañero

---
+++ src/test/java/com/ust/calc/multiplicacion/MultiplicacionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ust.calc.multiplicacion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiplicacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
