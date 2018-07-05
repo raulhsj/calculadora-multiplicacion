ef95ba13fcdf1a08097411f53d4b0b0e507369b4
raulhsj <Raul.Hernandez@ust-global.com>
2018-07-05 11:29:14 +0200

    Añadida directiva implements de la interfaz Multiply src/main/java/com/ust/calc/multiplicacion/controllers/MultiplicacionController.java

--- src/main/java/com/ust/calc/multiplicacion/controllers/MultiplicacionController.java
+++ src/main/java/com/ust/calc/multiplicacion/controllers/MultiplicacionController.java
@@ -3,6 +3,12 @@ package com.ust.calc.multiplicacion.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MultiplicacionController {
+public class MultiplicacionController implements Multiply{
+
+	@Override
+	public Integer doMulti(Integer one, Integer two) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
