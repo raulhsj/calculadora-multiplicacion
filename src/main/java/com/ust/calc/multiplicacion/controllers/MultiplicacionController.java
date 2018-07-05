2fc553b60b3e21ec38874ef51e5ee84b30dcd7b2
raulhsj <Raul.Hernandez@ust-global.com>
2018-07-05 11:55:38 +0200

    src\main\java\com\ust\calc\multiplicacion\controllers\MultiplicacionController.java: added logic to doMulti operation

--- src/main/java/com/ust/calc/multiplicacion/controllers/MultiplicacionController.java
+++ src/main/java/com/ust/calc/multiplicacion/controllers/MultiplicacionController.java
@@ -1,14 +1,17 @@
 package com.ust.calc.multiplicacion.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MultiplicacionController implements Multiply{
 
 	@Override
-	public Integer doMulti(Integer one, Integer two) {
+	@GetMapping("/multiply")
+	public Integer doMulti(@RequestParam(name = "one") Integer one, @RequestParam(name = "two") Integer two) {
 		// TODO Auto-generated method stub
-		return null;
+		return one * two;
 	}
 
 }
